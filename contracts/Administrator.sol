6e16ac3490dbdaff714c6e9985ee12b014cf8784
seon-2 <phia130@naver.com>
2022-12-24 22:39:58 +0900

    펀딩 관리를 address 배열로 수정

--- contracts/Administrator.sol
+++ contracts/Administrator.sol
@@ -7,10 +7,8 @@ import "./ChangesFunding.sol";
 
 contract Administrator {
   
-    ProjectFunding pF; // 프로젝트 펀딩 컨트랙트형 변수 pF
-    mapping (address => ProjectFunding.funding) pFundings; // 프로젝트 펀딩 모음
-    ChangesFunding cF; // 잔돈 펀딩 컨트랙트형 변수 cF
-    mapping (address => ChangesFunding.funding) cFundings; // 잔돈 펀딩 모음
+    address[] public pFundings; // 프로젝트 펀딩 CA 모음 
+    address[] public cFundings; // 잔돈 펀딩 CA 모음
 
     struct Donator { // 기부자 정보
         address donatorAddr;
@@ -21,27 +19,13 @@ contract Administrator {
     mapping (address => Donator) donators; // 기부자들 모음
 
     // 프로젝트 펀딩 등록 - 프로젝트 펀딩 컨트랙트에서 펀딩 생성 시 작동 (최초 1회)
-    function registerPFunding(ProjectFunding.funding memory _newFunding) public {
-        pF = ProjectFunding(msg.sender); // msg.sender는 각 프로젝트 펀딩 컨트랙트 주소
-        pFundings[msg.sender] = _newFunding;
-    }
-
-    // 프로젝트 펀딩 정보 가져오기
-    function getPFunding(address _pFAddr) public returns (ProjectFunding.funding memory){
-        pF = ProjectFunding(_pFAddr);
-        return pF.getFunding();
+    function registerPFunding() public {
+        pFundings.push(msg.sender);
     }
 
     // 잔돈 펀딩 등록 - 프로젝트 펀딩 컨트랙트에서 펀딩 생성 시 작동 (최초 1회)
-    function registerCFunding(ChangesFunding.funding memory _newFunding) public {
-        cF = ChangesFunding(msg.sender); // msg.sender는 각 프로젝트 펀딩 컨트랙트 주소
-        cFundings[msg.sender] = _newFunding;
-    }
-
-    // 잔돈 펀딩 정보 가져오기
-    function getCFunding(address _cFAddr) public returns (ChangesFunding.funding memory){
-        cF = ChangesFunding(_cFAddr);
-        return cF.getFunding();
+    function registerCFunding() public {
+        cFundings.push(msg.sender);
     }
 
     // 기부자 등록 -> 맨 처음 기부할 때 한 번만
