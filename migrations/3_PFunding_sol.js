b90cdb04c53c9d408c9157ac1ae4954055f07a2a
seon-2 <phia130@naver.com>
2023-01-07 02:29:35 +0900

    ProjectFunding 배포 파일 작성

--- migrations/3_PFunding_sol.js
+++ migrations/3_PFunding_sol.js
@@ -1,5 +1,5 @@
 const pF = artifacts.require("ProjectFunding");
 
 module.exports = function (deployer) {
-  deployer.deploy(pF, "0xd32Aa1518aAF1e60C5076EbeEEb3992f022878A4");
+  deployer.deploy(pF, "0x30eDdf84A2bdE0F26457a727775C826892D11ef9");
 };
