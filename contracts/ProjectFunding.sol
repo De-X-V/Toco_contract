ac8b4fccd897ed5f3fba044b9eb7e79ae1059f52
seon-2 <phia130@naver.com>
2023-01-07 02:29:19 +0900

    ProjectFunding 작성

--- contracts/ProjectFunding.sol
+++ contracts/ProjectFunding.sol
@@ -8,10 +8,12 @@ contract ProjectFunding {
   
     Administrator public admin;
     address owner;
+    address payable adminCA;
 
-    constructor(address _adminAddr) {
+    constructor(address payable _adminAddr) {
         owner = msg.sender;
         admin = Administrator(_adminAddr);
+        adminCA = _adminAddr;
     }
 
     modifier onlyOwner() {
@@ -21,10 +23,8 @@ contract ProjectFunding {
     
     struct funding {
         string fundingName; // 펀딩 이름
-        uint targetAmount; // 목표 모금액
-        uint currentAmount; // 현재 모금액
-        uint fundingStartDate; // 펀딩 시작일(unix time) 예) 12/18 22:00
-        uint fundingEndDate; // 펀딩 종료일(unix time) 예)  12/31 23:59
+        uint fundingStartDate; // 펀딩 시작일(unix time) 
+        uint fundingEndDate; // 펀딩 종료일(unix time) 
     }   
 
     funding public newFunding; // funding구조체형 변수 newFunding 선언
@@ -32,20 +32,19 @@ contract ProjectFunding {
     // 펀딩 설정
     function setFunding( // 펀딩 생성 시 입력할 값
         string memory _fundingName, // 펀딩 이름
-        uint _targetAmount, // 목표 모금액 -> 이더 단위로 입력
         uint _fundingStartDate, 
         uint _fundingEndDate 
     ) public {
         newFunding = funding({
             fundingName: _fundingName,
-            targetAmount: _targetAmount * 10 ** 18,
-            currentAmount: 0,
             fundingStartDate: _fundingStartDate,
             fundingEndDate: _fundingEndDate
         });
+    }
 
-        // 운영진 컨트랙트에 만든 펀딩 추가
-        admin.registerPFunding();
+    // 모금 시간 연장
+    function extendEndDate(uint _fundingEndDate) public onlyOwner {
+        newFunding.fundingEndDate = _fundingEndDate;
     }
 
     // 펀딩 정보 출력하는 함수 -> tuple로 출력
@@ -58,18 +57,20 @@ contract ProjectFunding {
 
     // 기부하는 기능 - wei 단위로
     function donate(uint _amount) public payable { // 각 펀딩 컨트랙트에 기부금이 쌓임
-        // 기부금이 0원 이상, 입력값이랑 같은지 확인
-        require(msg.value > 0 && msg.value == _amount);
+        // 기부금이 0원 이상
+        require(msg.value > 0);
         // 현재 시간이 기부 시작일과 종료일 사이 인지 확인
         require(block.timestamp > newFunding.fundingStartDate && block.timestamp < newFunding.fundingEndDate);
-        // 기부
-        donateList[msg.sender] += _amount;
-
-        // 해당 펀딩의 currentAmount 올려주기
-        newFunding.currentAmount += _amount;
+        
         // donator 정보 추가
         admin.setDonator(msg.sender);
-        admin.addDonatedPFList(msg.sender);
+        // 최초 한 번만 리스트에 해당 펀딩 추가
+        if(donateList[msg.sender] == 0) {
+            admin.addDonatedPFList(msg.sender);
+        }
+        
+        // 기부
+        donateList[msg.sender] += _amount;
     }
 
     // 컨트랙트 쌓인 잔고 보여주는 함수
@@ -77,8 +78,17 @@ contract ProjectFunding {
         return address(this).balance;
     }
 
+    // 나의 기부액 출력
+    function getMyDonateAmount() public view returns (uint) {
+        return donateList[msg.sender];
+    }
+
     // 컨트랙트 쌓인 잔고 출금 함수 (오직 owner만)
     function withdrawMoney(address payable _to) public onlyOwner {
-        _to.transfer(getBalance());
+        // 쌓인 잔고의 95% 송금
+        _to.transfer((getBalance() * 95) / 100);
+
+        // 나머지 운영비 5% admin으로 송금
+        adminCA.transfer((getBalance()));
     }
 }
