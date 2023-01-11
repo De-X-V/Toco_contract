f892cccc279b19065312200ea8ef583c503134a4
seon-2 <phia130@naver.com>
2023-01-12 03:05:41 +0900

    Admin 컨트랙트 수정 및 재배포

--- contracts/Administrator.sol
+++ contracts/Administrator.sol
@@ -6,11 +6,21 @@ import "./ProjectFunding.sol";
 import "./ChangesFunding.sol";
 
 contract Administrator {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
 
     struct Donator { // 기부자 정보
         address donatorAddr;
-        address[] myDonatedPFList; // 기부한 프로젝트 펀딩 컨트랙트 리스트
-        address[] myDonatedCFList; // 기부한 잔돈 펀딩 컨트랙트 리스트
+        string[] myDonatedPFList; // 기부한 프로젝트 펀딩 리스트 (DB의 ID)
+        string[] myDonatedCFList; // 기부한 잔돈 펀딩 컨트랙트 리스트
     }
 
     mapping (address => Donator) donators; // 기부자들 모음
@@ -27,17 +37,32 @@ contract Administrator {
     }
 
     // 기부 시 기부한 펀딩 리스트에 추가(ProjectFunding 컨트랙트에서 사용)
-    function addDonatedPFList(address _addr) public {
-        donators[_addr].myDonatedPFList.push(msg.sender);
+    function addDonatedPFList(address _addr, string memory _docId) public {
+        donators[_addr].myDonatedPFList.push(_docId);
     }
 
-    function addDonatedCFList(address _addr) public { // (ChangesFunding 컨트랙트에서 사용)
-        donators[_addr].myDonatedCFList.push(msg.sender);
+    function addDonatedCFList(address _addr, string memory _docId) public { // (ChangesFunding 컨트랙트에서 사용)
+        donators[_addr].myDonatedCFList.push(_docId);
     }
 
     // 기부한 프로젝트 펀딩 컨트랙트 리스트 출력
-    function getDonatedPFList(address _addr) public view returns (address[] memory) {
+    function getDonatedPFList(address _addr) public view returns (string[] memory) {
         return donators[_addr].myDonatedPFList;
+    }
+
+    function getDonatedCFList(address _addr) public view returns (string[] memory) {
+        return donators[_addr].myDonatedCFList;
+    }
+
+    // 컨트랙트 쌓인 잔고 보여주는 함수
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    // 컨트랙트 쌓인 잔고 출금 함수 (오직 owner만)
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+        require(getBalance() > _amount);
+        _to.transfer(_amount);
     }
 
     receive() external payable {} // admin 컨트랙트를 payable하게 바꾸어주는 코드
